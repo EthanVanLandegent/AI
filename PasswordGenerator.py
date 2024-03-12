50fda3d3a84b30cdc996166c148e0b4ad98bb915
VanLandegent <02ethan.vanlandegent@gulllakecs.org>
2024-03-12 14:17:53 -0400

    Made a condense verion of UserPassowrd - Encrypt using SHA256

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -2,3 +2,15 @@
 # Date: 3.12.2024
 # Program: Password Generator
 # Resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
+
+import hashlib
+import getpass
+
+def main():
+    password = getpass.getpass("Enter your password: ")
+    salt = "somerandomsalt"  # Change this to a random value
+    hashed_password = hashlib.sha256((password + salt).encode()).hexdigest()
+    print("Hashed password:", hashed_password)
+
+if __name__ == "__main__":
+    main()
