2d70843839941594d3597dd65c432b5ce66b65b6
VanLandegent <02ethan.vanlandegent@gulllakecs.org>
2024-03-14 14:01:00 -0400

    Printing names index Pre ChatGPT

---
+++ PrintingNamesIndex.py
@@ -0,0 +1,11 @@
+# Programmer: Ethan VanLandegent
+# Date: 3.14.2024
+# Program: Printing Names Index
+# Resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
+
+names = ["Corey","Chris","Travis","Dave"]
+
+index = 0
+for name in names:
+    print(index,name)
+    index += 1
